a6dec25913f6b198736e201373fcedb2e8660186
Alam Rodriguez <alamrd2016@gmail.com>
2023-09-08 14:17:17 -0400

    fixing things 1

--- src/components/inicio/menu-footer/MenuFooter.jsx
+++ src/components/inicio/menu-footer/MenuFooter.jsx
@@ -8,27 +8,27 @@ import Iconos from './Iconos';
 
 const MenuFooter = () => {
   return (
-    <footer className='position-fixed start-0 bottom-0 bg-dark w-100 py-2' style={{height:55}}>
+    <footer className='position-fixed start-0 bottom-0 bg-black w-100 py-2' style={{height:55}}>
       <nav className='d-flex justify-content-around'>
 
         <Iconos
-          icon={<AiFillHome className='display-5' />}
+          icon={<AiFillHome className='fs-2' />}
           text='Inicio'
         />
         <Iconos
-          icon={<AiFillHome className='display-5' />}
+          icon={<AiFillHome className='fs-2' />}
           text='Amigos'
         />
         <Iconos
-          icon={<AiFillHome className='display-5' />}
+          icon={<AiFillHome className='fs-2' />}
           text=''
         />
         <Iconos
-          icon={<AiFillHome className='display-5' />}
+          icon={<AiFillHome className='fs-2' />}
           text='Bandeja de entrada'
         />
         <Iconos
-          icon={<AiFillHome className='display-5' />}
+          icon={<AiFillHome className='fs-2' />}
           text='Perfil'
         />
 
