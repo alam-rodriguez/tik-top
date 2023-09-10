f6cd0d777edc39af7422053f6484bc2a465701d6
Alam Rodriguez <alamrd2016@gmail.com>
2023-09-09 23:52:57 -0400

    fixing things 13

--- src/components/inicio/video-render/Video.jsx
+++ src/components/inicio/video-render/Video.jsx
@@ -88,7 +88,7 @@ const Video = ({id, video, title, subtitle, videoAudioName, likes, comments, add
 
   return (
     <div className='position-sticky div-reproductor-video'>
-      <video className={`reproductor-video reproductor-video-${id} w-100 object-fit-cover`} muted autoPlay playsInline loop onClick={handleVideoClick} >
+      <video className={`reproductor-video reproductor-video-${id} w-100 object-fit-cover`} muted autoPlay playsInline loop onClick={handleVideoClick} style={{}} >
         <source src={video} type="video/mp4" />
         Tu navegador no admite la reproducción de videos.
       </video>
