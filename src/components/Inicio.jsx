031c70f92066b669e56477ab8af4f1629cf22493
Alam Rodriguez <alamrd2016@gmail.com>
2023-09-10 17:51:22 -0400

    fixing things 26

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -76,7 +76,7 @@ const Inicio = () => {
       <main className='inicio container p-0 bg-dark text-white position-relative'>
 
 
-<section style={{height:'90%'}}>
+<section style={{height:'100%'}}>
   {/* <Player>
   <source src="https://www.youtube.com/watch?v=RB5tR_nqUEw" />
 </Player> */}
